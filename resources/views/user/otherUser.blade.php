e976915b6d77dd9a15298c47748ddb3f373b272f
giankyle989 <kylelaguerta989@gmail.com>
2023-04-15 20:50:32 +0800

    Added post to other users

--- resources/views/user/otherUser.blade.php
+++ resources/views/user/otherUser.blade.php
@@ -1,4 +1,12 @@
 @include('partials.header')
 <x-navbar :user="$user"/>
 <x-otherUser_info :user="$user"/>
+@if ($user->posts->count() > 0)
+<h2 class="font-semibold ml-1">Posts</h2>
+    <x-post_list>
+        @foreach($user->posts as $post)
+        <x-post :user="$user" :post="$post" :comments="$post->post_comment"/>
+        @endforeach
+    </x-post_list>
+    @endif
 @include('partials.footer')
